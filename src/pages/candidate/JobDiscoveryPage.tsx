6a33129647d20651b8aaa12175ae537c9bf6c332
Vishnu Kumar <161946782+Vishnukrchy@users.noreply.github.com>
2025-06-07 12:34:11 +0530

    Fix Button component import statements

--- src/pages/candidate/JobDiscoveryPage.tsx
+++ src/pages/candidate/JobDiscoveryPage.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Search, MapPin, Clock, DollarSign, Building2, Filter, Heart, ExternalLink, Briefcase, Users, Star } from 'lucide-react';
-import { Card } from '../../components/common/Card';
-import { Button } from '../../components/common/Button';
-import { Input } from '../../components/common/Input';
+import Card from '../../components/common/Card';
+import Button from '../../components/common/Button';
+import Input from '../../components/common/Input';
 import Badge from '../../components/common/Badge';
 
 interface Job {
